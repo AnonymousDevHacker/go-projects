1faa551a7b108e725ceb23348eab562378bad27c
anonymousdevhacker <southbrazilchapter@novacrypt.org>
2020-10-28 11:21:07 +0000

    Calculator First Version

--- 01-Calculator/main.go
+++ 01-Calculator/main.go
@@ -3,10 +3,18 @@ package main
 import "fmt"
 
 func main() {
+	var num1 int
+	var num2 int
+	var operator string
 	fmt.Println("Calculator")
 	fmt.Println("Choose an operation:")
 	fmt.Println(` + - Plus
  - - Minus
  * - Multiply
  / - Divide`)
+	fmt.Scanln(&operator)
+	if operator == "+"{
+		result := num1 + num2
+		fmt.Println(result)
+}
 }
